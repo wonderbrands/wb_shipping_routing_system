6b92801cf1a4edef981cc4d719bc77dbdfff6871
SergioWB <sergio@wonderbrands.co>
2025-01-17 10:19:16 -0600

    [UPD] Update fields and view

--- shipping_routing_system/models/sale_order_extension.py
+++ shipping_routing_system/models/sale_order_extension.py
@@ -3,12 +3,35 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    # Campos calculados para mostrar la información del producto
+    product_name = fields.Char(string="Nombre del Producto", compute="_compute_product_info", store=True)
+    product_dimensions = fields.Char(string="Dimensiones del Paquete", compute="_compute_product_info", store=True)
+
     wb_srs_flag = fields.Boolean(string='Shipping Routing Flag', default=False, readonly=True)
     wb_shipping_options = fields.One2many(
         comodel_name='sale.order.shipping.option',
         inverse_name='sale_order_id',
         string='Shipping Options'
     )
+
+    @api.depends('order_line.product_id', 'order_line.product_uom_qty')
+    def _compute_product_info(self):
+        for order in self:
+            if order.order_line:
+                # Extraemos los datos del primer producto en las líneas de la orden
+                product = order.order_line[0].product_id
+                product_length = product.product_length
+                product_width = product.product_width
+                product_height = product.product_height
+                product_weight = product.product_weight
+                quantity = order.order_line[0].product_uom_qty
+
+                # Concatenamos las dimensiones y la cantidad
+                order.product_name = product.name
+                order.product_dimensions = f"Long: {product_length} cm, Width: {product_width} cm, Height: {product_height} cm, Weight: {product_weight} kg, Quantity: {quantity} units"
+            else:
+                order.product_name = ""
+                order.product_dimensions = ""
 
     @api.model
     def create(self, vals):
