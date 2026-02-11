20dc23af94fd2d0f4419642442a1f9210c86cc31
Sergio Gil <sergio@wonderbrands.co>
2026-02-10 22:50:45 -0600

    [UPD] Update to V18.0

--- shipping_routing_system/__manifest__.py
+++ shipping_routing_system/__manifest__.py
@@ -1,16 +1,20 @@
-# -*- coding: utf-8 -*-
-{
+{   
     'name': 'Shipping Routing Module',
-    'version': '1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Select carrier module',
     'description': 'This module selects the best option in a carriers list.',
-    'author': '"Sergio Guerrero"',
+    'author': 'Sergio Guerrero',
     'depends': ['base', 'sale'],
     'data': [
         'security/ir.model.access.csv',
         'views/sale_order_extension_views.xml',
         'views/sale_order_shipping_option_view.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'shipping_routing_system/static/src/css/styles.css', 
+        ],
+    },
     'installable': True,
     'application': True,
     'auto_install': False,
